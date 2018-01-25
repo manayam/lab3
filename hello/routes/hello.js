99043c78fafdb5935df76c64bf8515dbfacc47df
Ryan Manalastas <rmtmanalastas@gmail.com>
2018-01-25 10:09:53 -0800

    localhost3000

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,13 @@
  */
 
 exports.view = function(req, res){
-  res.render('index', {
-  	'name': '',
+	var Ryan = req.params.userName;
+	if (Ryan == undefined){
+		Ryan = 'World';
+	}
+
+	console.log('Name is ' + Ryan);
+	res.render('index', {
+  	'name': Ryan
   });
 };
